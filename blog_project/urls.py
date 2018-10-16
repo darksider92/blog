000433a05a31734ebcaafc61ab884c91ec3cd185
Petr Kuznetsov <darksider92@mail.ru>
2018-10-16 09:06:04 +0600

    Forms

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('post/new/', views.BlogCreateView.as_view(), name='post_new'),
     path('post/<int:pk>/edit/',
          views.BlogUpdateView.as_view(), name='post_edit'),
+
     path('post/<int:pk>/delete/',
          views.BlogDeleteView.as_view(), name='post_delete'),
 ]
